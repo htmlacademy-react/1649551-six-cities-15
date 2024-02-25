0a5bdae07866377b55e4c916ac74938581d78597
Belov Dmitrii <belovd90@mail.ru>
2024-02-25 16:30:44 +0300

    feat: add pages and components to project

---
+++ src/consts/consts.ts
@@ -0,0 +1,14 @@
+import { CitiesType } from '../types/types';
+
+export const Settings = {
+  OffersCount: 10
+};
+
+export const cities: CitiesType = [
+  'Paris',
+  'Cologne',
+  'Brussels',
+  'Amsterdam',
+  'Hamburg',
+  'Dusseldorf'
+];
