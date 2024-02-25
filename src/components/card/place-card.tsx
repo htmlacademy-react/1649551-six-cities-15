b59c87b7e454dca564a99279f64e0a2ad39365a3
Belov Dmitrii <belovd90@mail.ru>
2024-02-25 18:37:38 +0300

    feat: add Header component

--- src/components/card/place-card.tsx
+++ src/components/card/place-card.tsx
@@ -4,12 +4,17 @@ function PlaceCard(offers: PlaceCardType): JSX.Element {
   const {isPremium, previewImage, price, rating, title, type} = offers;
   return(
     <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>{isPremium ? 'Premium' : ''}</span>
-      </div>
+      {isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
+          <img style={{width: '260px', height: '200px'}}
+            className="place-card__image"
+            src={previewImage}
+            alt="Place image"
+          />
         </a>
       </div>
       <div className="place-card__info">
